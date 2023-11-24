30b1acefea7d4bf732a69377909dbe7caa16a891
Young soo <lovepys01@naver.com>
2023-11-24 23:38:00 +0900

    온습도 센서 작동을 위한 아두이노 코드 작성

---
+++ ondo/dht11.ino
@@ -0,0 +1,26 @@
+#include <DHT.h>
+
+#define DHTPIN 2    
+#define DHTTYPE DHT11   
+
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop() {
+  delay(100);
+
+  float h = dht.readHumidity();
+  float t = dht.readTemperature();
+
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  Serial.println(h);
+  Serial.println(t);
+}
